882955d6d6985cf9fac09fc3848ff48ba3996f80
Joaquin Segovia Light-it <joaquin.segovia@lightit.io>
2025-04-04 22:49:24 -0300

    day5

--- resources/views/Components/side-nav/side-nav.blade.php
+++ resources/views/Components/side-nav/side-nav.blade.php
@@ -1,5 +1,5 @@
 <nav>
     <x-side-nav.nav-link href="/" style="color: green">Home</x-side-nav.nav-link>
-    <x-side-nav.nav-link href="/about">About</x-side-nav.nav-link>
-    <x-side-nav.nav-link href="/contact">Contact</x-side-nav.nav-link>
+    <x-side-nav.nav-link href="about">About</x-side-nav.nav-link>
+    <x-side-nav.nav-link href="contact">Contact</x-side-nav.nav-link>
 </nav>
